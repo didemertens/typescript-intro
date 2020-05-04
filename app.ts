ea05ff1ca05096b468b29442849ba35b923de98c
didemertens <d.g.j.mertens@gmail.com>
2020-05-04 17:02:56 +0100

    typescript objects

--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-console.log('code here!')
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   if (showResult) {
+//     return `${phrase} ${n1 + n2}`
+//   }
+//   return 'Nothing to see here'
+// }
+
+// const number1 = '5'
+// const number2 = 2.9
+// const printResult = true
+// const phrase = 'The result is'
+
+// const result = add(+number1, +number2, printResult, phrase)
+
+// console.log(result)
+
+const person = {
+  name: 'Pooh',
+  age: 100
+}
+
+console.log(person.nickname)
