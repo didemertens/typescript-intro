c4f0ec796d6de8b7565ebabf3fc76d28b6cfde50
didemertens <d.g.j.mertens@gmail.com>
2020-05-04 15:18:09 +0100

    set up project

--- ts-only.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var firstNumber = document.getElementById("num1");
-var secondNumber = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+firstNumber.value, +secondNumber.value));
-});
